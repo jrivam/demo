0e48d3d7439ea50e3bfe5c3168289621161b7802
jorge riva <jrivam@hotmail.com>
2018-06-12 19:54:17 -0500

    Changes refactoring syntaxsign

---
+++ source/0.library/Impl/2.Data/Sql/Builder/AbstractSqlSyntaxSign.cs
@@ -0,0 +1,110 @@
+﻿using library.Interface.Data.Sql;
+using System.Configuration;
+using System;
+
+namespace library.Impl.Data.Sql.Builder
+{
+    public abstract class AbstractSqlSyntaxSign : ISqlSyntaxSign
+    {
+        protected string _parameterprefix;
+        public abstract string ParameterPrefix
+        {
+            get;
+        }
+
+        protected string _parameterseparator;
+        public virtual string ParameterSeparator
+        {
+            get
+            {
+                if (_parameterseparator == null)
+                    _parameterseparator = ConfigurationManager.AppSettings["sql.parameter.separator"] ?? "";
+
+                return _parameterseparator;
+            }
+        }
+
+        protected string _parameterassignment;
+        public abstract string ParameterAssignment
+        {
+            get;
+        }
+
+
+        protected string _aliasseparatorcolumn;
+        public virtual string AliasSeparatorColumn
+        {
+            get
+            {
+                if (_aliasseparatorcolumn == null)
+                    _aliasseparatorcolumn = ConfigurationManager.AppSettings["sql.alias.separator.column"] ?? ".";
+
+                return _aliasseparatorcolumn;
+            }
+        }
+
+        protected string _aliasseparatorcolumnkeyword;
+        public abstract string AliasSeparatorColumnKeyword
+        {
+            get;
+        }
+
+        protected string _aliasseparatortable;
+        public virtual string AliasSeparatorTable
+        {
+            get
+            {
+                if (_aliasseparatortable == null)
+                    _aliasseparatortable = ConfigurationManager.AppSettings["sql.alias.separator.table"] ?? "_";
+
+                return _aliasseparatortable;
+            }
+        }
+
+        protected string _aliasseparatortablekeyword;
+        public abstract string AliasSeparatorTableKeyword
+        {
+            get;
+        }
+
+
+        protected string _aliasenclosurecolumnopen;
+        public abstract string AliasEnclosureColumnOpen
+        {
+            get;
+        }
+
+        protected string _aliasenclosurecolumnclose;
+        public abstract string AliasEnclosureColumnClose
+        {
+            get;
+        }
+
+        protected string _aliasenclosuretableopen;
+        public abstract string AliasEnclosureTableOpen
+        {
+            get;
+        }
+
+        protected string _aliasenclosuretableclose;
+        public abstract string AliasEnclosureTableClose
+        {
+            get;
+        }
+
+        public abstract string GetOperator(WhereOperator whereoperator);
+
+        protected string _wherewildcardsingle;
+        public abstract string WhereWildcardSingle
+        {
+            get;
+        }
+
+        protected string _wherewildcardany;
+        public abstract string WhereWildcardAny
+        {
+            get;
+        }
+
+    }
+}
