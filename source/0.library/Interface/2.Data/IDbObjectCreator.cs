219942efc1fe5ee588b27825f6722c14729ff575
jorge riva <jrivam@hotmail.com>
2018-06-21 17:22:45 -0500

    Changes .

---
+++ source/0.library/Interface/2.Data/IDbObjectCreator.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace library.Interface.Data
+{
+    public interface IDbObjectCreator
+    {
+        DbProviderFactory ProviderFactory { get; }
+        IDbConnection Connection { get; }
+        IDbCommand Command { get; }
+        DbParameter Parameter { get; }
+        DbDataAdapter Adapter { get; }
+    }
+}
