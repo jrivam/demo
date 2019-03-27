5c7ba6c202a4b6b23f301d43ddb6956027caa5ac
jrivam <jrivam@gmail.com>
2019-03-27 16:44:43 -0500

    renaming projects

--- source/3.business/Properties/AssemblyInfo.cs
+++ source/3.business/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("domain")]
+[assembly: AssemblyTitle("business")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("domain")]
+[assembly: AssemblyProduct("business")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
