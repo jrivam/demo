3c6c1325a0d042a9740c91073dc4f29accc2b6bb
jrivam <jrivam@hotmail.com>
2020-06-05 22:14:48 +0200

    .

--- source/jrivam.Library/Impl/2.Persistence/Query/AbstractQueryData.cs
+++ source/jrivam.Library/Impl/2.Persistence/Query/AbstractQueryData.cs
@@ -95,37 +95,5 @@ namespace jrivam.Library.Impl.Persistence.Query
 
             return delete;
         }
-
-        //public virtual void Clear(U data)
-        //{
-        //    foreach (var column in data.Columns)
-        //    {
-        //        data[column.Description.Name].DbValue = null;
-        //    }
-        //}
-        //public virtual void Map(U data, int maxdepth = 1, int depth = 0)
-        //{
-        //    foreach (var property in typeof(U).GetPropertiesFromType(isprimitive: true, isforeign: true, attributetypes: new System.Type[] { typeof(DataAttribute) }))
-        //    {
-        //        if (property.isprimitive)
-        //        {
-        //            var entityproperty = typeof(T).GetPropertyFromType(property.info.Name);
-        //            data[property.info.Name].DbValue = entityproperty.GetValue(data.Entity);
-        //        }
-
-        //        if (property.isforeign)
-        //        {
-        //            depth++;
-        //            if (depth < maxdepth || maxdepth == 0)
-        //            {
-        //                var foreign = property.info.GetValue(this);
-        //                if (foreign != null)
-        //                {
-        //                    foreign.GetType().GetMethod("Map").Invoke(foreign, new object[] { foreign, maxdepth, depth });
-        //                }
-        //            }
-        //        }
-        //    }
-        //}
     }
 }
